28713f4a5d1a1a26acee016b363072aef9ba275c
MSalvaris <msalvaris@users.noreply.github.com>
2018-07-09 12:28:12 +0100

    Adds writing the json to file

--- experiments/gather_results.py
+++ experiments/gather_results.py
@@ -12,11 +12,15 @@ def read_json(filename):
     with open(filename) as f:
         return json.load(f)
 
+def write_json_to_file(json_data, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(json_data, outfile)
 
-def main():
+def main(filename='all_results.json'):
     files = iglob('**/results.json', recursive=True)
     json_data = (read_json(i) for i in files)
-    print(list(chain.from_iterable(json_data)))
+    write_json_to_file(list(chain.from_iterable(json_data)), filename)
+    logger.info('All results written to  {}'.format(filename))
 
 if __name__=="__main__":
     main()
